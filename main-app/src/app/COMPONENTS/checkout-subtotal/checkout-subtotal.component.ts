30fb14c7391da16411c9145ff2f50a22e6260102
samsonajulor <samson.ajulor@decagon.dev>
2022-02-07 00:00:48 +0100

    done with basic crud

--- main-app/src/app/COMPONENTS/checkout-subtotal/checkout-subtotal.component.ts
+++ main-app/src/app/COMPONENTS/checkout-subtotal/checkout-subtotal.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { ShoppingCartService } from 'src/app/SERVICES/shopping-cart.service';
 
 @Component({
   selector: 'app-checkout-subtotal',
   templateUrl: './checkout-subtotal.component.html',
-  styleUrls: ['./checkout-subtotal.component.scss']
+  styleUrls: ['./checkout-subtotal.component.scss'],
 })
 export class CheckoutSubtotalComponent implements OnInit {
+  constructor(public shoppingCart: ShoppingCartService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
